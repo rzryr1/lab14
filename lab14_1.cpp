bf0a67ec687facd4fa299a8e32fc10b2ef588138
rzryr1 <solotogre@gmail.com>
2025-01-24 18:06:39 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	T temp;
+	for(int i = 1; i < N; i++){
+		for(int j = i; j > 0; j--){
+			if(d[j] > d[j-1]){
+				temp = d[j];
+				d[j] = d[j-1];
+				d[j-1] = temp;
+			}
+		}
+		cout << "Pass " << i << ":";
+		for(int k = 0; k < N; k++) cout << d[k] << " ";
+		cout << "\n";
+	}
 
 }
 
